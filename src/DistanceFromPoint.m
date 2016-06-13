d21540dd3710a9f2aa594dbd0b941accc0dcc0f0
trenta3 <dario.balboni.96@gmail.com>
2016-06-13 16:09:15 +0200

    Scritte le prime documentazioni. Sto per iniziare il programma

---
+++ src/DistanceFromPoint.m
@@ -0,0 +1,10 @@
+% @input: curve is an array with parameter of a Fat Bèzier curve
+%         point is an array with point coordinates
+%         eps is the desidered precision: it is assured that the distance returned is
+%             at most eps far from the real distance
+% @output: d is the square of the distance between point and curve
+%          p is an array with minimal point coordinates
+
+function [d, p] = DistanceFromPoint (curve, point, eps)
+	
+endfunction
