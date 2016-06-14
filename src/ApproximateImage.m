3864e16f010d15c72d321bbf9d6f8e7b4ee72024
trenta3 <dario.balboni.96@gmail.com>
2016-06-14 17:11:05 +0200

    Aggiunto file per l'approssimazione delle immagini

---
+++ src/ApproximateImage.m
@@ -0,0 +1,8 @@
+% @input: imgname is the name (a string) of the image file to approximate
+%         n is the degree of the Bèzier curves to be used in approximating
+% @output: bezcurves is the matrix in which, in every row there is a bèzier curve approximating
+%                    the given image at the required precision
+
+function [bezcurves] = ApproximateImage (imgname, n)
+	% TODO: We have to write the while loop
+endfunction
