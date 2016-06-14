4e0016864caac9cfec5a71d1ab228b935259668f
trenta3 <dario.balboni.96@gmail.com>
2016-06-14 23:22:11 +0200

    Riscritta distanza da un punto solo per le curve di grado 3

--- src/DistanceFromPoint.m
+++ src/DistanceFromPoint.m
@@ -5,70 +5,64 @@
 % @output: d is the square of the distance between point and curve
 %          p is an array with minimal point coordinates
 
-function [d, px, py] = DistanceFromPoint (curve, point, eps)
-	debug = 0;
-	% That's the degree
-	n = ((size(curve, 2) - 1) ./ 2) - 1;
-
+% We write it for third degree bezier curves
+% l a_0 a_1 a_2 a_3 b_0 b_1 b_2 b_3
+% 1 2   3   4   5   6   7   8   9
+function [dist, minp] = DistanceFromPoint (curve, point, eps)
 	% We first check if the curve is flat enought
-	curveness = 0;
-	for i = 0:n
-		% For each coefficient of the curve we compute its distance from the "right" coefficient (as if the curve was a line)
-		ra = (curve(1, 2) * i + curve(1, n+2) * (n - i)) / n;
-		rb = (curve(1, n+3) * i + curve(1, 2*n+3) * (n - i)) / n;
-		curveness += (curve(1, i+2) - ra)^2 + (curve(1, n+i+3) - rb)^2;
-	endfor
-	if debug == 1
-		printf("Curveness: %e\t", curveness);
-		disp(curve);
-	endif
-	if curveness <= eps^2
-		% The curve is flat. We can stop the recursion and return the response for a line
-		up = (point(1, 1) - curve(1, 2)) * (curve(1, 2+n) - curve(1, 2)) + (point(1, 2) - curve(1, 3+n)) * (curve(1, 2*n+3) - curve(1, 3+n));
-		down = (curve(1, 2+n) - curve(1, 2))^2 + (curve(1, 2*n+3) - curve(1, 3+n))^2;
+	if sum((curve(1, 2:5) - (curve(1, 2)*[0 1 2 3] + curve(1, 5)*[3 2 1 0]) ./ 3).^2 + (curve(1, 6:9) - (curve(1, 6)*[0 1 2 3] + curve(1, 9)*[3 2 1 0]) ./3).^2 <= eps^2 * [1 1 1 1]) == 4
+		% The curve is flat so we calculate the minimum
+		Q = [curve(1, 5), curve(1, 9)];
+		P = [curve(1, 2), curve(1, 6)];
+		up = sum((point - Q) .* (P - Q));
+		down = sum((P - Q).^2);
 		t = up ./ down;
 		if t <= 0
 			t = 0;
 		elseif t >= 1
 			t = 1;
 		endif
-		px = t*curve(1, 2+n)+(1-t)*curve(1,2);
-		py = t*curve(1, 2*n+3)+(1-t)*curve(1,3+n);
-		d = (px - point(1,1))^2 + (py - point(1,2))^2;
-		if debug == 1
-			printf("distance: %e, point: (%e, %e)\n", d, px, py);
-		endif
-	else
-		% The curve is not flat, so we compute the two subcurves with de Casteljau algorithm and recurse
-		alpha = beta = [];
-		alpha(1, :) = curve(1, 2:(2+n));
-		beta(1, :) = curve(1, (3+n):(2*n+3));
-		for j = 1:n
-			for i = 0:(n-j)
-				alpha(j+1, i+1) = (alpha(j, i+1) + alpha(j, i+2))./2;
-				beta(j+1, i+1) = (beta(j, i+1) + beta(j, i+2))./2;
-			endfor
-		endfor
-		% So now the two curves are (control points)
-		c1 = c2 = [];
-		c1(1, 1) = c2(1, 1) = curve(1, 1);
-		for i = 0:n
-			c1(1, i+2) = alpha(i+1, 1);
-			c1(1, n+3+i) = beta(i+1, 1);
-			c2(1, i+2) = alpha(n-i+1, i+1);
-			c2(1, n+3+i) = beta(n-i+1, i+1);
-		endfor
-		[d1, px1, py1] = DistanceFromPoint(c1, point, eps);
-		[d2, px2, py2] = DistanceFromPoint(c2, point, eps);
 		
-		% And now we return the nearest point
-		if debug == 1
-			printf("Choose between d1 = %e, p1 = (%e, %e), d2 = %e, p2 = (%e, %e)\n", d1, px1, py1, d2, px2, py2);
-		endif
-		if d1 < d2
-			d = d1; px = px1; py = py1;
+		% We calculate the points on the line
+		minp = t*P + (1-t)*Q;
+		dist = sum((minp - point).^2);
+	else
+		% The curve is not flat, we subdivide it recursively using de Casteljau algorithm
+		mx = (curve(1, 3) + curve(1, 4))/2;
+		my = (curve(1, 7) + curve(1, 8))/2;
+		
+		l1x = (curve(1, 2) + curve(1, 3))/2;
+		l1y = (curve(1, 6) + curve(1, 7))/2;
+		
+		r2x = (curve(1, 4) + curve(1, 5))/2;
+		r2y = (curve(1, 8) + curve(1, 9))/2;
+		
+		l2x = (l1x + mx)/2;
+		l2y = (l1y + my)/2;
+		
+		r1x = (mx + r2x)/2;
+		r1y = (my + r2y)/2;
+		
+		l3x = (l2x + r1x)/2;
+		l3y = (l2y + r1y)/2;
+		
+		lcurve = [curve(1, 1),
+			curve(1, 2), l1x, l2x, l3x,
+			curve(1, 6), l1y, l2y, l3y];
+		
+		rcurve = [curve(1, 1),
+			l3x, r1x, r2x, curve(1, 5),
+			l3y, r1y, r2y, curve(1, 9)];
+		
+		% We then recurse over the two subcurves
+		[ldist, lminp] = DistanceFromPoint (lcurve, point, eps)
+		[rdist, rminp] = DistanceFromPoint (rcurve, point, eps)
+		
+		% And after recursing over the two subcurves we return the minimum of the two
+		if ldist < rdist
+			dist = ldist; minp = lminp;
 		else
-			d = d2; px = px2; py = py2;
+			dist = rdist; minp = rminp;
 		endif
 	endif
 endfunction
